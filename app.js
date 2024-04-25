00ad561bd9d3734d9370e1ab1aa2cecd2868f560
Dawson Knudtson <300078204@fvtc.edu>
2024-04-24 19:07:26 -0500

    added API-Router information

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const { appendFile } = require('fs')
-
-
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.static('public'));
-
 app.use(express.json());
+
+app.use(require('./routes/api-v1'))
 app.use(require('./routes/static'))
 
 app.listen(port, ()=> console.log(`Server is running at http://localhost:${port}`));
