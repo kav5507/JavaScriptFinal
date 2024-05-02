7bdaeb39fb1ee61d1de536bf0b0ad6b83971d0bf
Dawson Knudtson <300078204@fvtc.edu>
2024-05-01 19:28:19 -0500

    added some endpoint methods -- basic

--- routes/api-v1.js
+++ routes/api-v1.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const { getCollection, ObjectId } = require('../foodtruck-db')
 
+// get methods
 router.get('/api/menu', async (req, res) => {
     const collection = await getCollection('foodtruck-api', 'menu')
     const menu = await collection.find({}).toArray()
@@ -13,6 +14,87 @@ router.get('/api/events', async (req, res) => {
     const events = await collection.find({}).toArray()
     response.json(events)
 })
+
+//get by ID methods
+
+//menu 
+router.get('/api/menu/:id', async (req, res) => {
+    const { id } = req.params;
+    const collection = await getCollection('foodtruck-api', 'events');
+    const menu = await collection.findOne({ _id: new ObjectId(id) });
+
+});
+//events
+router.get('/api/events/:id', async (req, res) => {
+    const { id } = req.params;
+    const collection = await getCollection('foodtruck-api', 'events');
+    const event = await collection.findOne({ _id: new ObjectId(id) });
+
+});
+
+// post methods
+
+//menu
+router.post('/api/menu', async (request, response) => {
+    const { item } = request.body;
+    const collection = await getCollection('foodtruck-api', 'menu');
+    const result = await collection.insertOne({ /*add stuff here */ });
+});
+//events
+router.post('/api/events', async (request, response) => {
+    const { item } = request.body;
+    const collection = await getCollection('foodtruck-api', 'menu');
+    const result = await collection.insertOne({ /*add stuff here */ });
+});
+//put methods
+
+//menu
+router.put('/api/menu/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, description, price } = req.body;
+
+    const collection = await getCollection('foodtruck-api', 'menu');
+
+    
+        const result = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { /* add stuff here?? */}
+        )
+
+        
+});
+
+//events
+router.put('/api/events/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, location, dates, hours } = req.body;
+
+    const collection = await getCollection('foodtruck-api', 'menu');
+
+    
+        const result = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { /* add stuff here?? */}
+        )
+
+        
+});
+//delete methods
+
+//menu
+router.delete('/api/menu/:id', async (req, res) => {
+    const { id } = req.params;
+    const collection = await getCollection('foodtruck-api', 'menu');
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });  
+});
+
+//events
+router.delete('/api/events/:id', async (req, res) => {
+    const { id } = req.params;
+    const collection = await getCollection('foodtruck-api', 'menu');
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });  
+});
+
 
 
 
