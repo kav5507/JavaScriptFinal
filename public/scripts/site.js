68442034efe280b7b959bef8c3f4d670b3f29c8f
Ka <700195507@fvtc.edu>
2024-05-05 19:06:35 -0500

    added dawsons api-v1 endpoints

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,18 +1,26 @@
 let lasteventId = 0;
 
 const getEvents = async () => {
-	const response = await fetch('/api/events')
-	const { id, name, location, dates, hours } = await response.json()
-  }
-  
-  const getMenu = async () => {
+    const response = await fetch('/api/events')
+    const {_id,name,location,dates,hours} = await response.json()
+	
+	document.querySelector('.events h3').textContent = name
+	document.querySelector('.menu .location').textContent = location
+    document.querySelector('.menu .dates').textContent = dates
+	document.querySelector('.menu .hours').textContent = hours
+
+}
+
+
+const getMenu = async () => {
 	const response = await fetch('/api/menu')
 	const { _id, name, description, price } = await response.json()
-	ignoreId = _id;
+	ignoreId = _id
 	document.querySelector('.menu h5').textContent = name
 	document.querySelector('.menu .description').textContent = description
-	document.querySelector('.menu .price').textContent = price
-  }
-  
-  getMenu()
-  getEvents()
+    document.querySelector('.menu .price').textContent = price
+}
+
+
+getMenu()
+getEvents()
