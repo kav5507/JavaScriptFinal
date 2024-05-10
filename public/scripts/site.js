c59610a9a2f3b5703075cf23fbe741ced78b273a
Dawson Knudtson <300078204@fvtc.edu>
2024-05-10 17:36:59 -0500

    added Alex's code, final complete

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -2,14 +2,18 @@
 
 	const eventlist = document.querySelector('#events table')
 	const menutable = document.querySelector('#menu table')
+
 	
-	//#region GET and Display events
+
+	
+	
 	const getEvents = async () => {
 		const response = await fetch('/api/events')
 		const events = await response.json()
 		return events
 	}
 
+	//for index.html. Initially displays all events but with details hidden
 	const displayEventNames = async (events) => {
 		events.forEach(({_id, name, location, dates, hours}) => {
 			const tr = document.createElement('tr')	
@@ -52,15 +56,14 @@
 			eventlist.appendChild(tdhours)	
 		})
 	}
-	//#endregion
-
-	//#region GET and display menu
+	
 	const getMenu = async () => {
 		const response = await fetch('/api/menu')
 		const menu = await response.json()
 		return menu
 	}
 
+	//for menu.html
 	const displayMenuItems = async (menu) => {
 		menu.forEach(({_id, name, description, price}) => {
 			const tr = document.createElement('tr')
@@ -80,11 +83,9 @@
 		})
 	}
 
-	//#endregion
-
-	
 	displayEventNames(await getEvents())
 	displayMenuItems(await getMenu())
+
 
 	document.querySelectorAll('button').forEach((c) => {
 		c.onclick = function() {
@@ -96,19 +97,54 @@
 		}
 		
 	})
+	
+	//resets fields to autopopulate
+	document.querySelector('#txtEditMenuName').value = ""
+	document.querySelector('#txtEditMenuDescription').value = ""
+	document.querySelector('#txtEditMenuPrice').value = ""
+	document.querySelector('#txtEditEventName').value = ""
+	document.querySelector('#txtEditEventLocation').value = ""
+	document.querySelector('#txtEditEventDates').value = ""
+	document.querySelector('#txtEditEventHours').value = ""
+	document.getElementById('menuName-input').value = ""
+	document.getElementById('menuDescription-input').value = ""
+	document.getElementById('menuPrice-input').value = ""
+	document.getElementById('eventName-input').value = ""
+	document.getElementById('eventLocation-input').value = ""
+	document.getElementById('eventDates-input').value = ""
+	document.getElementById('eventHours-input').value = ""
 
+	/*
+	const clearTable = async () => {
+		const a = document.querySelector('#events')
+		const b = document.querySelector('table')
+		a.removeChild(b)
+	}
+	*/
+
+	//for admin.html. Gets names for update select options and displays
 	const getMenuSelectItems = async (menu) => {
 		const selectItem = document.querySelector('#selectItem')
-		menu.forEach(({_id, name}) => {
+		const option = document.createElement('option')
+		option.innerHTML = "Please select a menu item"
+		selectItem.appendChild(option)
+
+		menu.forEach(({_id, name, description, price}) => {
 			const option = document.createElement('option')
 			option.setAttribute("value", _id)
 			option.innerHTML = name
 			selectItem.appendChild(option)
 		})
+		
 	}
 
+	//for admin.html. Gets names for update select options and displays
 	const getEventSelectEvents = async (events) => {
 		const selectEvent = document.querySelector('#selectEvent')
+		const option = document.createElement('option')
+		option.innerHTML = "Please select an event"
+		selectEvent.appendChild(option)
+
 		events.forEach(({_id, name}) => {
 			const option = document.createElement('option')
 			option.setAttribute("value", _id)
@@ -117,18 +153,47 @@
 		})
 	}
 	 
+	//populates update fields based on selected option
 	const populateItems = async () => {
-		const itemName = document.querySelector('#menuName-input')
-		const id = document.querySelector('#selectItem').value
-		console.log(itemName, id)
+		const option = document.querySelector('#selectItem')
+		const id = option.options[option.selectedIndex].value
+	
+		if (id === 'Please select a menu item') { 
+			document.querySelector('#txtEditMenuName').value = ""
+			document.querySelector('#txtEditMenuDescription').value = ""
+			document.querySelector('#txtEditMenuPrice').value = ""
+			return
+		}
 		const response = await fetch(`/api/menu/${id}`)
-		console.log(response)
-		const {name, description, price } = await response.json()
-		console.log(name)
-		itemName.innerHTML = name
+		const item = await response.json()
+		document.querySelector('#txtEditMenuName').value = item.name
+		document.querySelector('#txtEditMenuDescription').value = item.description
+		document.querySelector('#txtEditMenuPrice').value = item.price
+
 	}
 
-	// added this for test
+	//populates update fields based on selected option
+	const populateEvents = async () => {
+		const option2 = document.querySelector('#selectEvent')
+		const id2 = option2.options[option2.selectedIndex].value
+
+		if (id2 === 'Please select an event') { 
+			document.querySelector('#txtEditEventName').value = ""
+			document.querySelector('#txtEditEventLocation').value = ""
+			document.querySelector('#txtEditEventDates').value = ""
+			document.querySelector('#txtEditEventHours').value = "" 
+			return
+		}
+		const response2 = await fetch(`/api/events/${id2}`)
+		const event = await response2.json()
+		document.querySelector('#txtEditEventName').value = event.name
+		document.querySelector('#txtEditEventLocation').value = event.location
+		document.querySelector('#txtEditEventDates').value = event.dates
+		document.querySelector('#txtEditEventHours').value = event.hours
+
+	}
+
+	/* added this for test
 	const populateMenuItemDetails = async () => {
 		const menuId = document.querySelector('#selectItem').value;
 		const response = await fetch(`/api/menu/${_id}`);
@@ -149,7 +214,10 @@
 		document.querySelector('#eventDates-input').value = dates;
 		document.querySelector('#eventHours-input').value = hours;
 	};
-	//#region INSERT/POST - menu
+	*/
+		
+	// Insert
+	
 	const addItem = async () => {
 		const name = document.getElementById('menuName-input').value;
 		const description = document.getElementById('menuDescription-input').value;
@@ -168,16 +236,36 @@
 		} else {
 			console.error('Failed');
 		}
+		document.getElementById('menuName-input').value = ""
+		document.getElementById('menuDescription-input').value = ""
+		document.getElementById('menuPrice-input').value = ""
+
+		//deletes and reportpulate select options with new item
+		const select = document.querySelector('#selectItem')
+		select.remove()
+
+		const nSelect = document.createElement('select')
+		nSelect.setAttribute('id', 'selectItem')
+		const form = document.querySelector('#formUpdateItem')
+		form.prepend(nSelect)
+		const selectItem = document.querySelector('#selectItem')
+		selectItem.addEventListener('change', populateItems)
+		document.querySelector('#txtEditMenuName').value = ""
+		document.querySelector('#txtEditMenuDescription').value = ""
+		document.querySelector('#txtEditMenuPrice').value = ""
+
+		getMenuSelectItems(await getMenu())
+		
+		
+
 	};
 	
-	document.querySelector('#formAddItem').addEventListener('submit', function(event) {
-		event.preventDefault(); // Prevent form submission -- not sure if this is really needed
+	document.querySelector('#formAddItem').addEventListener('submit', function(menu) {
+		menu.preventDefault(); // Prevent form submission -- not sure if this is really needed
 		addItem();
+
 	});
-	//#endregion
 	
-	
-	//#region  INSERT/POST - event
 	const addEvent = async () => {
 		const name = document.getElementById('eventName-input').value;
 		const location = document.getElementById('eventLocation-input').value;
@@ -197,25 +285,104 @@
 		} else {
 			console.error('Failed');
 		}
+
+		document.getElementById('eventName-input').value = ""
+		document.getElementById('eventLocation-input').value = ""
+		document.getElementById('eventDates-input').value = ""
+		document.getElementById('eventHours-input').value = ""
+
+		//deletes and reportpulate select options with new event
+		const select = document.querySelector('#selectEvent')
+		select.remove()
+
+		const nSelect = document.createElement('select')
+		nSelect.setAttribute('id', 'selectEvent')
+		const form = document.querySelector('#formUpdateEvent')
+		form.prepend(nSelect)
+		const selectItem = document.querySelector('#selectEvent')
+		selectItem.addEventListener('change', populateEvents)
+		document.querySelector('#txtEditEventName').value = ""
+		document.querySelector('#txtEditEventLocation').value = ""
+		document.querySelector('#txtEditEventDates').value = ""
+		document.querySelector('#txtEditEventHours').value = "" 
+
+		getEventSelectEvents(await getEvents())
 	};
-	
+
 	document.querySelector('#formAddEvent').addEventListener('submit', function(event) {
 		event.preventDefault(); // Prevent form submission -- not sure if this is really needed
 		addEvent();
+		
 	});
-	//#endregion
+	
+	//Update
 
-	//#region UPDATE/PUT - menu
+	const updateMenuItem = async () => {
+		const id = document.querySelector('#selectItem').value;
+		const name = document.getElementById('txtEditMenuName').value;
+		const description = document.getElementById('txtEditMenuDescription').value;
+		const price = document.getElementById('txtEditMenuPrice').value;
 
+		//updates DB 
+		const response = await fetch(`/api/menu/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name, description, price })
 
-	//#endregion
+			})
+			if (response.ok) {
+				console.log('Added');
+			} else {
+				console.error('Failed');
+			}
+
+			//sets select value to new name
+			const option = document.querySelector('#selectItem')
+			option.options[option.selectedIndex].innerHTML = name 
+	};
+
+	document.querySelector('#btnUpdateItem').addEventListener('click', function(event) {
+		event.preventDefault(); // Prevent form submission
+		updateMenuItem();
+	});
+
+	const updateEvent = async () => {
+		const id = document.querySelector('#selectEvent').value;
+		const name = document.getElementById('txtEditEventName').value;
+		const location = document.getElementById('txtEditEventLocation').value;
+		const dates = document.getElementById('txtEditEventDates').value;
+		const hours = document.getElementById('txtEditEventHours').value;
+
+		//updates DB
+		const response = await fetch(`/api/events/${id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name, location, dates, hours})
+
+			})
+			if (response.ok) {
+				console.log('Added');
+
+			} else {
+				console.error('Failed');
+			}
+		
+			//sets select value to new name
+			const option = document.querySelector('#selectEvent')
+			option.options[option.selectedIndex].innerHTML = name 
+	};
+
+	document.querySelector('#btnUpdateEvent').addEventListener('click', function(event) {
+		event.preventDefault(); // Prevent form submission
+		updateEvent();
+	});
 	
 
-
-	//#region DELETE - menu & event
+	// Delete
 
 	const deleteMenuItem = async () => {
-		const menuId = document.querySelector('#selectItem').value;
+		const menu = document.querySelector('#selectItem');
+		const menuId = menu.options[menu.selectedIndex].value
 		const response = await fetch(`/api/menu/${menuId}`, {
 			method: 'DELETE'
 		});
@@ -224,13 +391,22 @@
 			console.log('Menu item deleted successfully');
 			// Optionally, remove the item from the dropdown or refresh the list
 			document.querySelector(`#selectItem option[value="${menuId}"]`).remove();
+			document.querySelector('#txtEditMenuName').value = ""
+			document.querySelector('#txtEditMenuDescription').value = ""
+			document.querySelector('#txtEditMenuPrice').value = ""
 		} else {
 			console.error('Failed to delete the menu item');
 		}
 	};
+
+	document.querySelector('#btnDeleteItem').addEventListener('click', function(event) {
+		event.preventDefault(); // Prevent form submission
+		deleteMenuItem();
+	});
 	
 	const deleteEvent = async () => {
-		const eventId = document.querySelector('#selectEvent').value;
+		const item = document.querySelector('#selectEvent');
+		const eventId = item.options[item.selectedIndex].value
 		const response = await fetch(`/api/events/${eventId}`, {
 			method: 'DELETE'
 		});
@@ -239,34 +415,37 @@
 			console.log('Event deleted successfully');
 			// Optionally, remove the item from the dropdown or refresh the list
 			document.querySelector(`#selectEvent option[value="${eventId}"]`).remove();
+			document.querySelector('#txtEditEventName').value = ""
+			document.querySelector('#txtEditEventLocation').value = ""
+			document.querySelector('#txtEditEventDates').value = ""
+			document.querySelector('#txtEditEventHours').value = "" 
 		} else {
 			console.error('Failed to delete the event');
 		}
 	};
 	
-	//#endregion
-
-	
-	const btnAddItem = document.querySelector('#formAddItem')
-	const btnAddEvent = document.querySelector('#formAddEvent')
-	const selectItem = document.querySelector('#selectItem')
-	selectItem.addEventListener('change', populateItems)
-
-	const selectEvent = document.querySelector('#selectEvent')
-
-
-	document.querySelector('#btnDeleteItem').addEventListener('click', function(event) {
-		event.preventDefault(); // Prevent form submission
-		deleteMenuItem();
-	});
-	
 	document.querySelector('#btnDeleteEvent').addEventListener('click', function(event) {
 		event.preventDefault(); // Prevent form submission
 		deleteEvent();
 	});
-	
+
+	//for admin.html. Sets listener on selection change
+	const selectItem = document.querySelector('#selectItem')
+	selectItem.addEventListener('change', populateItems)
+
+	const selectEvent = document.querySelector('#selectEvent')
+	selectEvent.addEventListener('change', populateEvents)
+
+
 
 	getMenuSelectItems(await getMenu())
 	getEventSelectEvents(await getEvents())
+	
+	
+
+	
+	
+
+
 
 })()
--- routes/api-v1.js
+++ routes/api-v1.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const { getCollection, ObjectId } = require('../foodtruck-db')
 
-//#region GET methods
+// get methods
 router.get('/api/menu', async (req, res) => {
     const collection = await getCollection('foodtruck-api', 'menu')
     const menu = await collection.find({}).toArray()
@@ -14,107 +14,72 @@ router.get('/api/events', async (req, res) => {
     const events = await collection.find({}).toArray()
     res.json(events)
 })
-//#endregion
 
-//#region GET by ID methods
+//get by ID methods
 
 //menu 
 router.get('/api/menu/:id', async (req, res) => {
     const { id } = req.params;
     const collection = await getCollection('foodtruck-api', 'menu');
-    const menu = await collection.findOne({ "_id": new ObjectId(id) });
+    const menu = await collection.findOne({ _id: new ObjectId(id) });
     res.json(menu)
-})
-
+});
 //events
 router.get('/api/events/:id', async (req, res) => {
     const { id } = req.params;
     const collection = await getCollection('foodtruck-api', 'events');
     const events = await collection.findOne({ _id: new ObjectId(id) });
     res.json(events)
-})
-//#endregion
+});
 
-//#region POST Methods
-
+// post methods
 
 //menu
 router.post('/api/menu', async (req, res) => {
     // Finish this up at a later time -- @5/5/2024
-    const { name, description, price } = req.body;
+    const { name, description, price} = req.body;
     const collection = await getCollection('foodtruck-api', 'menu');
-    const result = await collection.insertOne({ name, description, price });
+    const result = await collection.insertOne({ _id: new ObjectId(), name, description, price})
     res.json(result)
-})
+});
 
 //events
 router.post('/api/events', async (req, res) => {
     const { name, location, dates, hours } = req.body;
     const collection = await getCollection('foodtruck-api', 'events');
-    const result = await collection.insertOne({ name, location, dates, hours });
-
+    const result = await collection.insertOne({ _id : new ObjectId(), name, location, dates, hours})
     res.json(result)
 
-})
-//#endregion
-
-//#region PUT methods
+});
+//put methods
 
 //menu
+
 router.put('/api/menu/:id', async (req, res) => {
     const { id } = req.params;
     const { name, description, price } = req.body;
+    const item = { name, description, price }
 
     const collection = await getCollection('foodtruck-api', 'menu');
-    const events = await collection.findOne({ _id: new ObjectId(id) });
 
-    
-        const result = await collection.updateOne(
-            {_id : new ObjectId(id)},
-            {
-                $set : {
-                    name : "tessssst",
-                    description: "tessssst",
-                    price: "3.99"
-                }
-            }
-           
-        )
+    const result = await collection.updateOne({_id : new ObjectId(id)}, {$set: item})    
+    res.json(result)
         
-        res.json(result)
-        
-})
+});
 
 //events
 router.put('/api/events/:id', async (req, res) => {
     const { id } = req.params;
     const { name, location, dates, hours } = req.body;
+    const event = { name, location, dates, hours }
 
     const collection = await getCollection('foodtruck-api', 'events');
-    const events = await collection.findOne({ _id: new ObjectId(id) });
 
-    
-        const result = await collection.updateOne(
-            {_id : new ObjectId(id)},
-            {
-                $set : {
-                    name : "Plover Pizza Place",
-                    location : "Plover",
-                    dates : "July 15, 2024",
-                    hours : "10am - 4pm"
-                }
-            }
-           
-        )
-        
-        res.json(result)
-        
+    const result = await collection.updateOne({_id : new ObjectId(id)}, {$set: event})    
+    res.json(result)
+                
 });
-
-//#endregion
-
-
-//#region DELETE methods
+//delete methods
 
 //menu
 router.delete('/api/menu/:id', async (req, res) => {
@@ -131,7 +96,7 @@ router.delete('/api/events/:id', async (req, res) => {
     const result = await collection.deleteOne({ _id: new ObjectId(id) });  
     res.json(result)
 });
-//#endregion
+
 
 
 
